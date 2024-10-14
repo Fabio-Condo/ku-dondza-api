93ddf693a6372e870923f8ff77164c59355c0ccc
Fábio Condo <fabioenosse59@gmail.com>
2024-10-14 08:51:21 +0200

    update

--- src/main/java/com/fabiocondo/repository/QuestionRepository.java
+++ src/main/java/com/fabiocondo/repository/QuestionRepository.java
@@ -1,6 +1,5 @@
 package com.fabiocondo.repository;
 
-import com.fabiocondo.domain.OnlineCourseContent;
 import com.fabiocondo.domain.Question;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;

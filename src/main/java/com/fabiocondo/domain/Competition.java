c0f25f1512e8192ed9dd790d4d6c5938637ed0bd
Fábio Condo <fabioenosse59@gmail.com>
2024-10-30 16:10:15 +0200

    update

--- src/main/java/com/fabiocondo/domain/Competition.java
+++ src/main/java/com/fabiocondo/domain/Competition.java
@@ -1,8 +1,5 @@
 package com.fabiocondo.domain;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
 import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
@@ -17,7 +14,8 @@ public class Competition {
 
     private String title;
 
-    @JsonIgnore
+    //@JsonIgnore
+    //@JsonIgnoreProperties({"competitions"})
     @ManyToMany
     @JoinTable(
             name = "competition_questions",
@@ -26,7 +24,8 @@ public class Competition {
     )
     private Set<Question> questions = new HashSet<>();
 
-    @JsonIgnore
+    //@JsonIgnore
+    //@JsonIgnoreProperties({"competitions"})
     @ManyToMany
     @JoinTable(
             name = "competition_users",

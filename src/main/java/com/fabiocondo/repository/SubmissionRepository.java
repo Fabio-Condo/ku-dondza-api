a8a0cd143b6c290f209c7f47495913b3b2d279e0
Fábio Condo <fabioenosse59@gmail.com>
2025-02-06 14:41:31 +0200

    update

--- src/main/java/com/fabiocondo/repository/SubmissionRepository.java
+++ src/main/java/com/fabiocondo/repository/SubmissionRepository.java
@@ -13,10 +13,8 @@ public interface SubmissionRepository extends JpaRepository<Submission, Long> {
     boolean existsByCompetitionAndUser(Competition competition, User user);
     Optional<Submission> findByUserIdAndCompetitionId(Long userId, Long competitionId);
     List<Submission> findByCompetitionId(Long competitionId);
-
     // Contar o número total de respostas para uma questão
     long countByAnswers_Question(Question question);
-
     // Consulta para contar respostas corretas associadas a uma questão
     public long countByAnswers_IsCorrectTrueAndAnswers_Question(Question question);
     public long countByAnswers_IsNullAndAnswers_Question(Question question);

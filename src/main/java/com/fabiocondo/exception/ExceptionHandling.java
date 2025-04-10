7dd5c6ce5b239277cfc79cc9217971c9ef6c88f6
Fábio Condo <fabioenosse59@gmail.com>
2025-04-10 21:23:28 +0200

    update

--- src/main/java/com/fabiocondo/exception/ExceptionHandling.java
+++ src/main/java/com/fabiocondo/exception/ExceptionHandling.java
@@ -8,6 +8,10 @@ import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.authentication.LockedException;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,11 +27,15 @@ import static org.springframework.http.HttpStatus.*;
 public class ExceptionHandling implements ErrorController {
 
     private final Logger LOGGER = LoggerFactory.getLogger(getClass());
+    private static final String ACCOUNT_LOCKED = "Your account has been locked. Please contact administration";
     private static final String METHOD_IS_NOT_ALLOWED = "This request method is not allowed on this endpoint. Please send a '%s' request";
     private static final String INTERNAL_SERVER_ERROR_MSG = "An error occurred while processing the request";
+    private static final String INCORRECT_CREDENTIALS = "Username / password incorrect. Please try again";
+    private static final String ACCOUNT_DISABLED = "Your account has been disabled. If this is an error, please contact administration";
     private static final String ERROR_PROCESSING_FILE = "Error occurred while processing file";
+    private static final String NOT_ENOUGH_PERMISSION = "You do not have enough permission";
     public static final String ERROR_PATH = "/error";
-    public static final String ENTITY_IN_USE = "Entity in use, can't be deleted";
+    private static final String ENTITY_IN_USE = "Entity in use, can't be deleted";
 
 
     @ExceptionHandler(EmailExistException.class)
@@ -108,6 +116,26 @@ public class ExceptionHandling implements ErrorController {
     @ExceptionHandler(CombinationExistException.class)
     public ResponseEntity<HttpResponse> combinationExistException(CombinationExistException exception) {
         return createHttpResponse(BAD_REQUEST, exception.getMessage());
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<HttpResponse> badCredentialsException() {
+        return createHttpResponse(BAD_REQUEST, INCORRECT_CREDENTIALS);
+    }
+
+    @ExceptionHandler(DisabledException.class)
+    public ResponseEntity<HttpResponse> accountDisabledException() {
+        return createHttpResponse(BAD_REQUEST, ACCOUNT_DISABLED);
+    }
+
+    @ExceptionHandler(LockedException.class)
+    public ResponseEntity<HttpResponse> lockedException() {
+        return createHttpResponse(UNAUTHORIZED, ACCOUNT_LOCKED);
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<HttpResponse> accessDeniedException() {
+        return createHttpResponse(FORBIDDEN, NOT_ENOUGH_PERMISSION.toUpperCase());
     }
 
     @ExceptionHandler(IllegalArgumentException.class)

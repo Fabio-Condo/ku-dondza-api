5b830c9acc9824f8003474f2391d4be55c8be642
Fábio Condo <fabioenosse59@gmail.com>
2025-05-16 19:05:22 +0200

    update

--- src/main/java/com/fabiocondo/KuDondzaApi.java
+++ src/main/java/com/fabiocondo/KuDondzaApi.java
@@ -36,7 +36,8 @@ public class KuDondzaApi {
                 "http://192.168.11.45",
                 "http://192.168.43.2",
                 "http://168.231.87.25",
-                "http://dikahub.com"
+                "http://dikahub.com",
+                "http://www.dikahub.com"
         ));
         corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
                 "Accept", "Jwt-Token", "Authorization", "Origin, Accept", "X-Requested-With",

bd3b35971532e0a6101c8ad1a5cd5183eda3ccc8
Fábio Condo <fabioenosse59@gmail.com>
2025-07-29 12:55:14 +0200

    update

--- src/main/java/com/fabiocondo/constant/EmailConstant.java
+++ src/main/java/com/fabiocondo/constant/EmailConstant.java
@@ -2,10 +2,13 @@ package com.fabiocondo.constant;
 
 public class EmailConstant {
     public static final String SIMPLE_MAIL_TRANSFER_PROTOCOL = "smtps";
-    public static final String USERNAME = "fabioenosse59@gmail.com";
-    public static final String PASSWORD = "rqreanrjkbvrtgjn";
-    public static final String FROM_EMAIL = "fabioenosse59@gmail.com";
-    public static final String CC_EMAIL = "fabioenossecondo@gmail.com";
+    //public static final String USERNAME = "fabioenosse59@gmail.com";
+    //public static final String PASSWORD = "rqreanrjkbvrtgjn";
+    public static final String USERNAME = "dikahub.education@gmail.com";
+    public static final String PASSWORD = "hvdigtxfzwrjwigu";
+    public static final String FROM_EMAIL = "dikahub.education@gmail.com";
+    //public static final String CC_EMAIL = "fabioenossecondo@gmail.com";
+    public static final String CC_EMAIL = "dikahub.education@gmail.com";
     public static final String EMAIL_SUBJECT = "Dikahub, Código de verificação";
     public static final String GMAIL_SMTP_SERVER = "smtp.gmail.com";
     public static final String SMTP_HOST = "mail.smtp.host";

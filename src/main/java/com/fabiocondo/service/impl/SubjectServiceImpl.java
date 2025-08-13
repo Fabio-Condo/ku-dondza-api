1f55b17d7947a0b4cc9d8976d177b7835219281b
Fábio Condo <fabioenosse59@gmail.com>
2025-08-13 15:51:58 +0200

    update

--- src/main/java/com/fabiocondo/service/impl/SubjectServiceImpl.java
+++ src/main/java/com/fabiocondo/service/impl/SubjectServiceImpl.java
@@ -131,7 +131,7 @@ public class SubjectServiceImpl implements SubjectService {
     @Override
     public Subject update(Subject subject, Long id) throws SubjectNotFoundException {
         Subject existSubject = findById(id);
-        //existSubject.setSubjectId(UUID.randomUUID().toString());
+        existSubject.setSubjectId(UUID.randomUUID().toString());
         BeanUtils.copyProperties(subject, existSubject, "id", "subjectId", "topics");
         logger.info("Updating subject: " + subject.getName());
         return subjectRepository.save(existSubject);

969d23217670fa2c65b004c88b2eec2fb1e596f7
Fábio Condo <fabioenosse59@gmail.com>
2026-04-09 17:34:50 +0200

    update

--- src/main/java/com/fabiocondo/enumeration/Institution.java
+++ src/main/java/com/fabiocondo/enumeration/Institution.java
@@ -3,7 +3,9 @@ package com.fabiocondo.enumeration;
 public enum Institution {
     UEM,
     UP,
-    //UJC,
-    //ACIPOL,
-    //AM; // ACADEMIA MILITAR
+    ISCISA,
+    ISCAM,
+    UJC,
+    ACIPOL,
+    AM; // ACADEMIA MILITAR
 }

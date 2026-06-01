bedcff8467806e0d00fc089e1892ba6f2c8f4280
Fábio Condo <fabioenosse59@gmail.com>
2026-06-01 16:25:29 +0200

    update

--- src/main/java/com/fabiocondo/controller/TopicController.java
+++ src/main/java/com/fabiocondo/controller/TopicController.java
@@ -37,6 +37,12 @@ public class TopicController {
         return ResponseEntity.status(HttpStatus.OK).body(topicService.findById(id));
     }
 
+    //@GetMapping("/find-by-topicId/{topicId}")
+    public ResponseEntity<Topic> findTopicByTopicId(@PathVariable("topicId") String topicId) throws TopicNotFoundException {
+        Topic topic = topicService.findTopicByTopicId(topicId);
+        return ResponseEntity.status(HttpStatus.OK).body(topic);
+    }
+
     @GetMapping("/find-by-topicId/{topicId}")
     public ResponseEntity<TopicDTO> findTopicByTopicId_WithCache(@PathVariable("topicId") String topicId) throws TopicNotFoundException {
         return ResponseEntity.status(HttpStatus.OK).body(topicService.findTopicByTopicId_WithCache(topicId));
